dbc254b0d846d3a21916b77847838bc620db7192
Sauron754 <advancedcode754@gmail.com>
2017-04-18 23:14:50 +0200

    added partial implementation for objects/celestialBodies.py

---
+++ SpaceScript/objects/celestialBodies.py
@@ -0,0 +1,5 @@
+class orbiatalObject():
+	def __init__(eccentricity_float, semiMajorAxis_arr, inclination_float, argumentOfPeriapsis_float, trueAnomaly_float, longitudeOfAscendingNode_float, referenceDirection_arr = [1, 0, 0]):
+
+class CMO():
+	def __init__(self, ):
