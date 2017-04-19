547b535d0cfcea020f61a255df3f96663a3aaca8
Sauron754 <advancedcode754@gmail.com>
2017-04-19 23:40:50 +0200

    deleted old __init__.py from data system

--- SpaceScript/data/__init__.py
+++ SpaceScript/data/__init__.py
@@ -1,8 +1,1 @@
 import os
-
-modulePath = os.path.dirname(exceptions.analysisExceptions.__file__)
-modulePathSegments = modulePath.split("/")
-subdirectoryCount = len(array)
-modulePathReal = []
-for iteration in range(subdirectoryCount - 1):
-	modulePathReal.append(modulePathSegments[iteration])
