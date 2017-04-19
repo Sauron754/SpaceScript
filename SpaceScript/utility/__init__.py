f6e68a7ce764d8eeeeeb06361c08b23f36bf7aae
Sauron754 <advancedcode754@gmail.com>
2017-04-20 01:25:03 +0200

    added import for needed modules

--- SpaceScript/utility/__init__.py
+++ SpaceScript/utility/__init__.py
@@ -1,1 +1,6 @@
+import SpaceScript
+from SpaceScript import exceptions
+from SpaceScript.exceptions import utility
+from SpaceScript.exceptions.utility import dimensionError
+
 __all__ = ['calcFunctions', 'constants', 'vectorAlgebra']
