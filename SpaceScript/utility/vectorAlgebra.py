622a94fa2d71ce145448c116c01368e73b7c1f1d
Sauron754 <advancedcode754@gmail.com>
2017-04-23 16:54:23 +0200

    added dimensionCheckFatal function to raise errors if vector dimension are not equal -> deleted dimension check inside functions

--- SpaceScript/utility/vectorAlgebra.py
+++ SpaceScript/utility/vectorAlgebra.py
@@ -1,13 +1,19 @@
-def add(A, B):
+import SpaceScript
+from SpaceScript import exceptions
+from SpaceScript.exceptions import utility
+
+def dimensionCheckFatal(A, B):
 	if len(A) != len(B):
-		raise dimensionError(
-			"Line 3 in <module> myArray", "vector dimensions not equal")
+		raise SpaceScript.exceptions.utility.dimensionError("vector dimensions 												   not equal")
+def add(A, B):
+	dimensionCheckFatal(A, B)
 	vectorOut = []
 	for iteration in range(len(A)):
 		vectorOut.append(A[iteration] + B[iteration])
 	return vectorOut
 
 def scalarProduct(A, B):
+	dimensionCheckFatal(A, B)
 	scalar = 0
 	for iteration in range(len(A)):
 		scalar += A[iteration] * B[iteration]
