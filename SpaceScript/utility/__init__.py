85321b23efc9d935e45a94886cc473f526d53cdb
Sauron754 <advancedcode754@gmail.com>
2017-04-23 17:23:09 +0200

    reconfigured __init__.py for utility package

--- SpaceScript/utility/__init__.py
+++ SpaceScript/utility/__init__.py
@@ -1,6 +1,1 @@
-import SpaceScript
-from SpaceScript import exceptions
-from SpaceScript.exceptions import utility
-from SpaceScript.exceptions.utility import dimensionError
-
 __all__ = ['calcFunctions', 'constants', 'vectorAlgebra']
