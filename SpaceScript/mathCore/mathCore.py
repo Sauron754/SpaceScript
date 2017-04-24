4e23f15c9ad6773a4243f08da2333e60977139c2
Taragor <Taragor83@gmail.com>
2017-04-24 20:38:00 +0200

    added spaces to =

--- SpaceScript/mathCore/mathCore.py
+++ SpaceScript/mathCore/mathCore.py
@@ -6,14 +6,14 @@ from SpaceScript.utility import constants
 def calcGravitationalInfluence(orbital_obj):
 	#calculates the gravitational influence of a Parent to a orbiting or passing body
 	ParentBody_obj = eval(orbital_obj.parent_str)
-	dist_arr=[0,0,0]
-	orbitalPos_arr=orbital_obj.position_arr
-	ParentPos_arr=ParentBody_obj.position_arr
-	if len(orbitalPos_arr)==len(ParentPos_arr):
-		iteration_int=0
+	dist_arr = [0,0,0]
+	orbitalPos_arr = orbital_obj.position_arr
+	ParentPos_arr = ParentBody_obj.position_arr
+	if len(orbitalPos_arr) == len(ParentPos_arr):
+		iteration_int = 0
 		for iteration_int in range(len(orbitalPos_arr)):
-			dist_arr[iteration_int]=ParentPos_arr[iteration_int]-orbitalPos_arr[iteration_int]
-	gForce_float = G*((orbital_obj.mass_int*ParentBody_obj.mass_int)/SpaceScript.utility.vectorAlgebra.length(dist_arr))
+			dist_arr[iteration_int] = ParentPos_arr[iteration_int]-orbitalPos_arr[iteration_int]
+	gForce_float = SpaceScript.utility.constants.G*((orbital_obj.mass_int*ParentBody_obj.mass_int)/SpaceScript.utility.vectorAlgebra.length(dist_arr))
 	return gForce_float
 	
 def calcOrbitalPosition(orbital_obj, time_float):
@@ -21,13 +21,13 @@ def calcOrbitalPosition(orbital_obj, time_float):
 	momVelocity_arr = orbital_obj.getVelocity()
 	momPos_arr = orbital_obj.position_arr
 	force_arr = calcGravitationalInfluence(orbital_obj)
-	iteration_int=0
+	iteration_int = 0
 	for iteration_int in range(len(force_arr)):
-		velocityAdd_arr[iteration_int]=time_float*(force_arr[iteration_int]/orbital_obj.mass_int)
-	newOrbitalVelocity_arr=SpaceScript.utility.vectorAlgebra.add(velocityAdd_arr, momVelocity_arr)
-	iteration_int=0
+		velocityAdd_arr[iteration_int] = time_float*(force_arr[iteration_int]/orbital_obj.mass_int)
+	newOrbitalVelocity_arr = SpaceScript.utility.vectorAlgebra.add(velocityAdd_arr, momVelocity_arr)
+	iteration_int = 0
 	for iteration_int in range(len(newOrbitalVelocity_arr)):
-		deltaPos_arr[iteration_int]=time_float*newOrbitalVelocity_arr[iteration_int]
-	momPos_arr= SpaceScript.utility.vectorAlgebra.add(deltaPos_arr, momPos_arr)
+		deltaPos_arr[iteration_int] = time_float*newOrbitalVelocity_arr[iteration_int]
+	momPos_arr = SpaceScript.utility.vectorAlgebra.add(deltaPos_arr, momPos_arr)
 	orbital_obj.setVelocity(newOrbitalVelocity_arr)
 	return momPos_arr
