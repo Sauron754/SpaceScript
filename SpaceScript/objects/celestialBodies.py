b0aaa7a751855d673b68260a0f2da7ed360465fe
Sauron754 <advancedcode754@gmail.com>
2017-04-24 15:04:12 +0200

    changed position object properties

--- SpaceScript/objects/celestialBodies.py
+++ SpaceScript/objects/celestialBodies.py
@@ -1,7 +1,7 @@
 class orbiatalObject():
 	def __init__(self, eccentricity_float, semiMajorAxis_arr, 
 				 inclination_float, argumentOfPeriapsis_float,
-				 trueAnomaly_float,
+				 trueAnomaly_float, position_arr,
 				 longitudeOfAscendingNode_float, parent_str,
 				 referenceDirection_arr = [1, 0, 0]):
 		self.eccentricity_float = eccentricity_float
@@ -9,22 +9,23 @@ class orbiatalObject():
 		self.inclination_float = inclination_float
 		self.argumentOfPeriapsis_float = argumentOfPeriapsis_float
 		self.trueAnomaly_float = trueAnomaly_float
+		self.position_arr = position_arr
 		self.longitudeOfAscendingNode_float = longitudeOfAscendingNode_float
 		self.parent_str = parent_str
 		self.referenceDirection_arr = referenceDirection_arr
 
-	def getPosition(self, parentPosition_arr):
+	def setPosition(self, parentPosition_arr, time_int):
 
 
 class CMO(orbiatalObject):
 	def __init__(self, eccentricity_float, semiMajorAxis_arr,inclination_float,
-				 argumentOfPeriapsis_float, trueAnomaly_float,
+				 argumentOfPeriapsis_float, trueAnomaly_float, position_arr,
 				 longitudeOfAscendingNode_float, mass_int, surfaceRadius_int,
 				 rotation_arr, parent_str,
 				 atmosphereRadius_int = 0, atmospheric_bool = False,
 				 referenceDirection_arr = [1, 0, 0]):
 		super().__init__(eccentricity_float, semiMajorAxis_arr, inclination_float,
-				 argumentOfPeriapsis_float, trueAnomaly_float,
+				 argumentOfPeriapsis_float, trueAnomaly_float, position_arr,
 				 longitudeOfAscendingNode_float, parent_str,
 				 referenceDirection_arr)
 		self.mass_int = mass_int
