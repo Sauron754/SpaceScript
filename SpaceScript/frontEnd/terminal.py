553a571cf1f37256f551ed84d8b610d5a9ee55cc
Sauron754 <advancedcode754@gmail.com>
2017-04-25 11:13:16 +0200

    added terminal function

--- SpaceScript/frontEnd/terminal.py
+++ SpaceScript/frontEnd/terminal.py
@@ -2,4 +2,10 @@ def clientTerminal(out_str):
 	print(out_str)
 	command_str = input()
 	return command_str
-def terminal(option_int):
+def terminal(option_int, out_str = 'none'):
+	if option_int == 0:
+		if out_str == 'none':
+			command_str = clientTerminal('Welcome to SpaceScript!!!')
+		else:
+			command_str = clientTerminal(out_str)
+		return command_str
