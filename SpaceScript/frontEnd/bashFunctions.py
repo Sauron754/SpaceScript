fe084b0fed6023017e70a6ec9f6b96bea6241e48
Sauron754 <advancedcode754@gmail.com>
2017-04-29 08:32:48 +0200

    added save and load functions

--- SpaceScript/frontEnd/bashFunctions.py
+++ SpaceScript/frontEnd/bashFunctions.py
@@ -1,2 +1,18 @@
+import SpaceScript
+from SpaceScript import data
+from SpaceScript.data import dataHandler
+
 def help(*args):
 	#when finished will return help for desired command
+
+def new(*args):
+	if args[0] == "save":
+		SpaceScript.data.dataHandler.newWorldSave(args[1])
+	#append list of all 'new' options
+
+def load(*args):
+	if args[0] == "world":
+		#now we need to add a save reading mechanism here or maybe we might open
+		#a new .py file in the data package which will then handle the saving
+		#writing routines, then we only need to call this module when we want
+		#to perform a sandardized load/save
