84cde6d9fe85f0d73ef3513e46694b14d8101115
Sauron754 <advancedcode754@gmail.com>
2017-04-29 09:46:53 +0200

    added functionality test for dimensionCheck

--- testEnv/data/vectorAlgebraFunctional.py
+++ testEnv/data/vectorAlgebraFunctional.py
@@ -6,17 +6,43 @@ from SpaceScript import utility
 from SpaceScript.utility import vectorAlgebra
 
 def dimensionCheckFatal():
-	argumentsValid = [[1, 2], [2, 1]]
-	argumentsInvalid = [[1, 2], [1, 2, 3]]
-	runthroughs = []
-	try:
-		SpaceScript.utility.vectorAlgebra.dimensionCheckFatal(argumentsValid[0]
-															  argumentsValid[1])
-		runthroughs.append(True)
-	except:
-		runthroughs.append(False)
+	argumentsValid_arr = [[1, 2], [2, 1]]
+	argumentsInvalid_arr = [[1, 2], [1, 2, 3]]
+	runthroughs_arr = []
 	try:
 		SpaceScript.utility.vectorAlgebra.dimensionCheckFatal(
-			argumentsInvalid[0], argumentsInvalid[1])
-		runthroughs.append(False)
+			argumentsValid_arr[0], argumentsValid_arr[1])
+		runthroughs_arr.append(True)
+	except:
+		runthroughs_arr.append(False)
+	try:
+		SpaceScript.utility.vectorAlgebra.dimensionCheckFatal(
+			argumentsInvalid_arr[0], argumentsInvalid_arr[1])
+		runthroughs_arr.append(False)
 	except dimensionError:
+		runthroughs_arr.append(True)
+	except:
+		runthroughs_arr.append(False)
+	for element in runthroughs_arr:
+		if element != True:
+			return False
+	else:
+		return True
+
+def dimensionCheck():
+	argumentsValid_arr = [[1, 2], [2, 1]]
+	argumentsInvalid_arr = [[1, 2], [1, 2, 3]]
+	runthroughs_arr = []
+	if SpaceScript.utility.vectorAlgebra.dimensionCheck(argumentsValid_arr) == True:
+		runthroughs_arr.append(True)
+	else:
+		runthroughs_arr.append(False)
+	if SpaceScript.utility.vectorAlgebra.dimensionCheck(argumentsInvalid_arr) == False:
+		runthroughs_arr.append(True)
+	else:
+		runthroughs_arr.append(False)
+	for element in runthroughs_arr:
+		if element != True:
+			return False
+	else:
+		return True
