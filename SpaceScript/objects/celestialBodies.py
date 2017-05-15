272f9114288d716c5a11c9754cca4cd7b06348c5
Sauron754 <advancedcode754@gmail.com>
2017-05-15 21:55:48 +0200

    reworked orbitalObject class and deleted CMO

--- SpaceScript/objects/celestialBodies.py
+++ SpaceScript/objects/celestialBodies.py
@@ -10,42 +10,32 @@ vectorLength = SpaceScript.utility.vectorAlgebra.length()
 vectorInvert = SpaceScript.utility.vectorAlgebra.invert()
 vectorAdd = SpaceScript.utility.vectorAlgebra.add()
 
-class orbiatalObject():
-	def __init__(self, eccentricity_float, semiMajorAxis_arr, 
-				 inclination_float, argumentOfPeriapsis_float,
-				 trueAnomaly_float, position_arr,
-				 longitudeOfAscendingNode_float, parent_str,
-				 referenceDirection_arr = [1, 0, 0]):
-		self.eccentricity_float = eccentricity_float
+class orbitalObject():
+	"""class that descibes mechanics of orbital objects"""
+	def __init__(self, parent_str, position_arr, velocity_arr, 
+				 acceleration_arr, ascendingNode_arr = [], 
+				 semiMajorAxis_arr = [], semiMinorAxis = [], 
+				 inclination_float = False,
+				 longitudeOfAscendingNode_float = False, 
+				 argumentOfPeriapsis_float = False, trueAnomaly_float = False,
+				 eccentricity_float = False, distanceFromParent_float = False):
+		self.parent_str = parent_str
+		self.position_arr = position_arr
+		self.velocity_arr = velocity_arr
+		self.acceleration_arr = acceleration_arr
+		self.ascendingNode_arr = ascendingNode_arr
 		self.semiMajorAxis_arr = semiMajorAxis_arr
+		self.semiMinorAxis_arr = semiMinorAxis_arr
 		self.inclination_float = inclination_float
+		self.longitudeOfAscendingNode_float = longitudeOfAscendingNode_float
 		self.argumentOfPeriapsis_float = argumentOfPeriapsis_float
 		self.trueAnomaly_float = trueAnomaly_float
-		self.position_arr = position_arr
-		self.longitudeOfAscendingNode_float = longitudeOfAscendingNode_float
-		self.parent_str = parent_str
-		self.referenceDirection_arr = referenceDirection_arr
+		self.eccentricity_float = eccentricity_float
+		self.distanceFromParent_float = distanceFromParent_float
+		self.kepplerValidity = False
 
-class CMO(orbiatalObject):
-	def __init__(self, eccentricity_float, semiMajorAxis_arr,inclination_float,
-				 argumentOfPeriapsis_float, trueAnomaly_float, position_arr,
-				 longitudeOfAscendingNode_float, mass_int, surfaceRadius_int,
-				 rotation_arr, parent_str,
-				 atmosphereRadius_int = 0, atmospheric_bool = False,
-				 referenceDirection_arr = [1, 0, 0]):
-		super().__init__(eccentricity_float, semiMajorAxis_arr,
-						 inclination_float, argumentOfPeriapsis_float,
-						 trueAnomaly_float, position_arr,
-						 longitudeOfAscendingNode_float, parent_str,
-						 referenceDirection_arr)
-		self.mass_int = mass_int
-		self.radius_int = radius_int
-		self.rotation_arr = rotation_arr
+	def kepplerUpdate():
+		self.kepplerValidity = True
 
-	def getSphereOfInfluence(self, object_obj):
-		influence = False
-		distance = vectorLength(self.semiMajorAxis_arr) * (self.mass_int / object_obj.mass_int)**(2/5)
-		relativePosition_arr = vectorAdd(object_obj.position_arr, vectorInvert(slef.position_arr))
-		if (relativePosition_arr[0]**2 + relativePosition_arr[1]**2 + relativePosition_arr[2]**2 < distance**2):
-			influence = True
-		return influence
+	def impulseSystemChange():
+		self.kepplerValidity = False
