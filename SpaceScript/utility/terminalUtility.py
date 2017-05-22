7d2709bb25dbab6002d97400967362713d806cb7
Sauron754 <advancedcode754@gmail.com>
2017-05-22 21:21:07 +0200

    corrected minor discrepancy concerning the argumentCountCheck function

--- SpaceScript/utility/terminalUtility.py
+++ SpaceScript/utility/terminalUtility.py
@@ -3,7 +3,7 @@ from SpaceScript import exceptions
 from SpaceScript.exceptions import frontEnd
 from SpaceScript.exceptions.frontEnd import argumentCountError as argumentCountError
 
-def argumentCountCheck(argumentNumber, *args):
+def argumentCountCheck(argumentNumber):
 	if argumentNumber !== len(args):
 		raise argumentCountError("Invalid Argument Count", "Please retry!")
 		return False
