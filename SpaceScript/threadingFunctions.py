8d3188cb48810df07a501835f40955524b5f551c
Sauron754 <advancedcode754@gmail.com>
2017-05-23 11:02:44 +0200

    added lowTermThread function

--- SpaceScript/threadingFunctions.py
+++ SpaceScript/threadingFunctions.py
@@ -1,4 +1,14 @@
-def simThread(queues, pipes, holdValue):
+import SpaceScript
+import multiprocessing
+from SpaceScript import frontEnd
+from SpaceScript.frontEnd import terminal as terminal
+
+def simThread(queues_arr, pipes_arr, holdValue, objectArray_arr):
 
 
-def termThread(queues, pipes, holdValue):
+def termThread(queues_arr, pipes_arr, holdValue, objectArray_arr):
+	initialized_bool = True
+
+	while initialized_bool:
+		
+def lowTermThread(queues_arr)
