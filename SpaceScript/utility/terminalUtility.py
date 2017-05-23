b499d8c7aebd78ded5d738bff2ea4ce333f2f88b
Sauron754 <advancedcode754@gmail.com>
2017-05-23 10:41:30 +0200

    corrected minor variable naming discrepancy

--- SpaceScript/utility/terminalUtility.py
+++ SpaceScript/utility/terminalUtility.py
@@ -3,8 +3,8 @@ from SpaceScript import exceptions
 from SpaceScript.exceptions import frontEnd
 from SpaceScript.exceptions.frontEnd import argumentCountError as argumentCountError
 
-def argumentCountCheck(argumentNumber_int, *args):
-	if argumentNumber_int !== len(args):
+def argumentCountCheck(argumentNumber_int, arguments_arr):
+	if argumentNumber_int !== len(arguments_arr):
 		raise argumentCountError("Invalid Argument Count", "Please retry!")
 		return False
 	else:
