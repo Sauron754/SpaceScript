e0ca82a18a0e0385a54bca3de2b2af7c93dd7be5
Sauron754 <advancedcode754@gmail.com>
2017-05-24 15:55:17 +0200

    added return to lowTermThread

--- SpaceScript/threadingFunctions.py
+++ SpaceScript/threadingFunctions.py
@@ -15,3 +15,4 @@ def lowTermThread(outString_str, mode_int, parentHoldValue_v, stringPipe_p):
 	inString_str = terminal(mode_int, outString_str)
 	stringPipe_p.send(inString_str)
 	parentHoldValue_v.value = 1
+	return inString_str
