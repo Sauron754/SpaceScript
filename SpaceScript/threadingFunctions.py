79faa76cb48832eea8b645c46f51333b2b6ded0e
Sauron754 <advancedcode754@gmail.com>
2017-05-24 13:00:31 +0200

    finished lowTermThread function

--- SpaceScript/threadingFunctions.py
+++ SpaceScript/threadingFunctions.py
@@ -3,12 +3,15 @@ import multiprocessing
 from SpaceScript import frontEnd
 from SpaceScript.frontEnd import terminal as terminal
 
-def simThread(queues_arr, pipes_arr, holdValue, objectArray_arr):
+def simThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr, mainLock):
 
 
-def termThread(queues_arr, pipes_arr, holdValue, objectArray_arr):
+def termThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr, mainLock):
 	initialized_bool = True
 
 	while initialized_bool:
 		
-def lowTermThread(queues_arr)
+def lowTermThread(outString_str, mode_int, parentHoldValue_v, stringPipe_p):
+	inString_str = terminal(mode_int, outString_str)
+	stringPipe_p.send(inString_str)
+	parentHoldValue_v.value = 1
