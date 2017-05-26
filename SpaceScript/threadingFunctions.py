a506226d58d0b0d5c298862bf7cc2a7874a21783
Sauron754 <advancedcode754@gmail.com>
2017-05-26 09:04:53 +0200

    extended termThread

--- SpaceScript/threadingFunctions.py
+++ SpaceScript/threadingFunctions.py
@@ -9,8 +9,11 @@ def simThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr, mainLock):
 def termThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr, mainLock):
 	initialized_bool = True
 	lowTermThreadHold_v = multiprocessing.Value("i")
+	messageQueue_q = queues_arr[0]
 
 	while initialized_bool:
+		message = messageQueue_q.get()
+		IOThread = multiprocessing.Process(target = lowTermThread, args = ())
 		
 def lowTermThread(outString_str, mode_int, parentHoldValue_v, stringPipe_p):
 	inString_str = terminal(mode_int, outString_str)
