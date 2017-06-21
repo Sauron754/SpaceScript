8fb2eaf7c3b3f858103a025b3ccbe9223ff90909
Sauron754 <advancedcode754@gmail.com>
2017-06-21 17:04:57 +0200

    implemented termThreadControlHandler

--- SpaceScript/threadingFunctions.py
+++ SpaceScript/threadingFunctions.py
@@ -7,6 +7,7 @@ from SpaceScript.frontEnd import terminal as terminal
 from SpaceScript.utility import terminalUtility
 from SpaceScript.utility.terminalUtility import safePull as safePull
 from SpaceScript.utility.terminalUtility import termThreadEventHandler as termThreadEventHandler
+from SpaceScript.utility.terminalUtility import termThreadControlHandler as termThreadControlHandler
 from appJar import gui
 
 def simThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr = None,
@@ -16,15 +17,19 @@ def simThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr = None,
 def termThread(queues_arr, pipes_arr, holdValue_v, objectArray_arr = None,
 			   mainLock = None):
 	commandPipe = pipes_arr[0]
+	controlQueue_q = queues_arr[0]
 	pullString_q = multiprocessing.Queue()
 	pushString_q = multiprocessing.Queue()
 	termThreadHold_v = multiprocessing.Value()
+	guiHold_v = multiprocessing.Value()
 	termThreadHold_v.value() = False
 	subProcess = multiprocessing.Process(target = terminal, args = (
 										pullString_q, pushString_q,
 										termThreadHold_v))
 	subProcess.start()
-	eventHandler_bool = False
-	while False:
+	checkSequence_bool = True
+	while checkSequence_bool:
 		termThreadEventHandler(termThreadHold_v, pullString_q, commandPipe,
 							   holdValue_v)
+		termThreadControlHandler(termThreadHold_v, controlQueue_q, pushString_q,
+								 guiHold_v)
