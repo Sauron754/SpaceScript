a776e89e56522ab153328f0f7d6df66c79759440
Sauron754 <advancedcode754@gmail.com>
2017-06-21 12:36:57 +0200

    added termThreadControlHandler function

--- SpaceScript/utility/terminalUtility.py
+++ SpaceScript/utility/terminalUtility.py
@@ -19,7 +19,7 @@ def safePull(queue):
 	except queue.Empty:
 		return chr(0)
 
-def termThreadEventHandler(holdValue_v, pullString_q, commandPipe, ParentHoldValue_v):
+def termThreadEventHandler(holdValue_v, pullString_q, commandPipe_p, ParentHoldValue_v):
 	if holdValue_v.value == True:
 		commandString_str = safePull(pullString_q)
 		parsedCommandString_arr = commandString_str.split(" ")
@@ -27,9 +27,24 @@ def termThreadEventHandler(holdValue_v, pullString_q, commandPipe, ParentHoldVal
 		command_str = parsedCommandString_str[0] 
 		parsedCommandString_arr.remove(command_str)
 		arguments_arr = parsedCommandString_str
-		commandPipe.send([command_str, arguments_arr])
+		commandPipe_p.send([command_str, arguments_arr])
 		ParentHoldValue_v.value = 1
 		backgroundEventHandler_bool = True
+		holdValue_v.value = False
 		return True
+	else:
+		return False
+
+def termThreadControlHandler(holdValue_v, controlString_q, pushString_q, childHoldValue_v):
+	if holdValue_v == True:
+		controlString_str = safePull(controlString_q)
+		controlSequence_arr = controlString_str.split(" ")
+		if controlSequence_arr[0] == "out":
+			pushString_q.put(controlSequence_arr[1])
+			childHoldValue_v.value = True
+			return True
+		else:
+			return False
+			#maybe add an error into here
 	else:
 		return False
