d3441a46031737f0d23709c0a79464fa2e13f363
Sauron754 <advancedcode754@gmail.com>
2017-06-21 20:58:01 +0200

    added terminalRefresh function and implemented it

--- SpaceScript/frontEnd/terminal.py
+++ SpaceScript/frontEnd/terminal.py
@@ -1,23 +1,36 @@
 from appJar import gui
 from multiprocessing import Queue
 
-def clientTerminal(pullString_q, pushString_q, holdValue_v):
+def clientTerminal(pullString_q, pushString_q, holdValue_v, ParentHoldValue_v):
 	global term
-	global transferQueue_q
+	global transferPullQueue_q
+	global transferPushQueue_q
+	global transferParentHold_v
 	global transferHold_v
+	transferParentHold_v = ParentHoldValue_v
 	transferHold_v = holdValue_v
-	transferQueue_q = pullString_q
+	transferPullQueue_q = pullString_q
+	transferPushQueue_q = pushString_q
 	term = gui()
 	term.addListBox("mainOutput", ["Big thank you to appJar", "Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>", "", "Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>", "For more information type license"])
 	term.addEntry("BASH")
 	term.enableEnter(submit)
+	term.registerEvent(terminalRefresh)
+	term.setPollTime(1)
 	app.go()
 
 def submit(key):
 	inputString_str = term.getEntry("BASH")
 	transferQueue_q.put(inputString_str)
-	holdValue_v.value = True
+	transferParentHold_v.value = True
 	return True
+
+def terminalRefresh():
+	if transferHold_v.value:
+		term.addListItems("BASH", [safePull(transferPushQueue_q)])
+		return True
+	else:
+		return False
 
 def terminal(option, pullString_q, pushString_q):
 	if option == 0:
