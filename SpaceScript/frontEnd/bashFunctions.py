c4d88c6e9d30ed215b36db43a00a2fce51e798f1
Sauron754 <advancedcode754@gmail.com>
2017-06-22 14:26:04 +0200

    added argument count check function to all bash functions

--- SpaceScript/frontEnd/bashFunctions.py
+++ SpaceScript/frontEnd/bashFunctions.py
@@ -3,23 +3,35 @@ from SpaceScript import data
 from SpaceScript import utility
 from SpaceScript.data import dataHandler
 from SpaceScript.utility import terminalUtility
+from SpaceScript.utility.terminalUtility import terminalOutput as terminalOutput
 
 
-maxArguments_dict = {"help": , "new": , "load":, }
+maxArguments_dict = {"help": 1, "new": , "load":, }
 
 def help(*args):
 	try:
-		SpaceScript.utility.terminalUtility.argumentCountCheck(maxArguments_dict["help"], args)
+		SpaceScript.utility.terminalUtility.argumentCountCheck(
+												maxArguments_dict["help"], args)
 	except argumentCountError:
-		#restartTerminal()
+		terminalOutput("Invalid number of Arguments")
 	#when finished will return help for desired command
 
 def new(*args):
+	try:
+		SpaceScript.utility.terminalUtility.argumentCountCheck(
+												maxArguments_dict["new"], args)
+	except argumentCountError:
+		terminalOutput("Invalid number of Arguments")
 	if args[0] == "save":
 		SpaceScript.data.dataHandler.newWorldSave(args[1])
 	#append list of all 'new' options
 
 def load(*args):
+	try:
+		SpaceScript.utility.terminalUtility.argumentCountCheck(
+												maxArguments_dict["load"], args)
+	except argumentCountError:
+		terminalOutput("Invalid number of Arguments")
 	if args[0] == "world":
 		#now we need to add a save reading mechanism here or maybe we might open
 		#a new .py file in the data package which will then handle the saving
