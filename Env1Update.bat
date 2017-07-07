67e9a9bcefc2dd42d347a5e4ef570576c3225fa7
Sauron754 <advancedcode754@gmail.com>
2017-07-07 22:50:53 +0200

    added code to update master branch to isolated test enviroment

---
+++ Env1Update.bat
@@ -0,0 +1,4 @@
+del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\Env1\* /F
+XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\SpaceScript\* C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\Env1 /F/E/R
+@echo DONE!
+pause
---
+++ testEnvUpdate.bat
@@ -0,0 +1,4 @@
+del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\compiledTestEnv\* /F
+XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\testEnv C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\compiledTestEnv/v/e/r
+@echo DONE!
+pause
