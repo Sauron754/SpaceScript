17eabe9989e75a2d8dc9cfab017fd02ac7d193dc
Sauron754 <advancedcode754@gmail.com>
2017-07-09 00:26:14 +0200

    started working on clientTerminalIOTest function

--- testEnv/frontEnd/terminal.py
+++ testEnv/frontEnd/terminal.py
@@ -3,4 +3,12 @@ def clientTerminalIOTest():
 	#therefore it is not included in the standardized testing procedure
 	#if anything is modified in the terminal gui the dev working on it is asked
 	#to verify the codes integrity himself
-	
+	import multiprocessing
+	import SpaceScript
+	from multiprocessing import Processing, Query, Value
+	from SpaceScript import frontEnd
+	from SpaceScript.frontEnd import terminal
+	pullQueue_q = multiprocessing.Query()
+	pushQueue_q = multiprocessing.Query()
+	holdValue_v = multiprocessing.Value()
+	childHoldValue_v = multiprocessing.Value()
