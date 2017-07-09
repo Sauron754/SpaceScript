9bfd18363f1633c9fd843466b1604fdd8de49c49
Sauron754 <advancedcode754@gmail.com>
2017-07-09 22:32:13 +0200

    some shit got fucked up so i fixed it

--- testEnvironments/compiledTestEnv/testEnv/frontEnd/terminal.py
+++
@@ -1,6 +0,0 @@
-def clientTerminalIOTest():
-	#this test sequence can only be executed by a human
-	#therefore it is not included in the standardized testing procedure
-	#if anything is modified in the terminal gui the dev working on it is asked
-	#to verify the codes integrity himself
-	
