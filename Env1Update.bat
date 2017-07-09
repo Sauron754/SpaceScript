b2533b639ac2298a81d882b59cea6c954e20260f
Sauron754 <advancedcode754@gmail.com>
2017-07-09 22:45:01 +0200

    updated for name change

--- Env1Update.bat
+++ Env1Update.bat
@@ -1,4 +1,4 @@
-del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\Env1\* /F
-XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\SpaceScript\* C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\Env1 /F/E/R
+del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript\* /F
+XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\SpaceScript\* C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript /F/E/R
 @echo DONE!
 pause
