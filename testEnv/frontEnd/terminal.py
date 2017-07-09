e7386818e2662ecf3f51a08a4fa39613deabfd67
Sauron754 <advancedcode754@gmail.com>
2017-07-09 21:58:55 +0200

    finished clientTerminalIOTest function

--- testEnv/frontEnd/terminal.py
+++ testEnv/frontEnd/terminal.py
@@ -5,10 +5,17 @@ def clientTerminalIOTest():
 	#to verify the codes integrity himself
 	import multiprocessing
 	import SpaceScript
-	from multiprocessing import Processing, Query, Value
+	from multiprocessing import Process, Query, Value
 	from SpaceScript import frontEnd
 	from SpaceScript.frontEnd import terminal
+	from SpaceScript.terminal import terminal as terminal
 	pullQueue_q = multiprocessing.Query()
 	pushQueue_q = multiprocessing.Query()
 	holdValue_v = multiprocessing.Value()
 	childHoldValue_v = multiprocessing.Value()
+	thread = multiprocessing.Process(target = terminal, args = (0, pullQueue_q,
+									 pushQueue_q, childHoldValue_v,
+									 holdValue_v))
+	print("You should now see this message displayed in the gui!")
+	pushQueue_q.put("You should now see this message displayed in the gui")
+	childHoldValue_v.value = True
