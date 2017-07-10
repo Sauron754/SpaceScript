85c108eb7ead703b31ef252afd7d1ea7848a7961
Sauron754 <advancedcode754@gmail.com>
2017-07-10 21:34:04 +0200

    added code to completely wipe directory before update

--- Env1Update.bat
+++ Env1Update.bat
@@ -1,5 +1,5 @@
-del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript\* /F
-rmdir C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript\*
+rmdir C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript\ /S
+mkdir C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript\
 XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\SpaceScript\* C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\SpaceScript /F/E/R
 @echo DONE!
 pause
