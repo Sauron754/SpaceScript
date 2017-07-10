d4f565074ef59a8277338cede5a8f636fec966b9
Sauron754 <advancedcode754@gmail.com>
2017-07-10 21:17:04 +0200

    added command to delete empty folder skeleton

--- testEnvUpdate.bat
+++ testEnvUpdate.bat
@@ -1,4 +1,5 @@
 del C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\testEnv\* /F
+rmdir C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\testEnv\*
 XCOPY C:\Data\Python_Library\SpaceScript\SpaceScript\testEnv C:\Data\Python_Library\SpaceScript\SpaceScript\testEnvironments\testEnv/v/e/r
 @echo DONE!
 pause
